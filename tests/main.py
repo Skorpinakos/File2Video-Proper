21335b7ce8eb39e127f84e2023df898eeabc8ef9
Ioannis Tsampras <gtsambras@gmail.com>
2023-02-28 02:52:13 +0200

    adding parity VP and str8 pipeline

--- tests/main.py
+++ tests/main.py
@@ -16,11 +16,12 @@ class Result():
         self.current_virtual_pixel=0
         self.padding=padding
         self.size=initial_size
+        self.virtual_pixels_per_frame=(x/8)*(y/8)
 
     def color_distr(self,number):
         return number*63
     def safe(self,string):
-        if string=='0':
+        if string=='0' or string=='':
             return '00000000'
         else:
             return string
@@ -39,10 +40,25 @@ class Result():
         self.create_virtual_pixel(packet_4)
         
     def create_virtual_pixel(self,color):
-        print(color)
-        rgb=[self.color_distr(int(color[0:2],2)),self.color_distr(int(color[2:4],2)),self.color_distr(int(color[4:],2))]
-        
-        print(rgb)
+        #print(color)
+        rgb=[self.color_distr(int(self.safe(color[0:2]),2)),self.color_distr(int(self.safe(color[2:4]),2)),self.color_distr(int(self.safe(color[4:]),2))]
+        if self.current_virtual_pixel==0:
+            self.add_parity_virtual_pixel(self.frames)
+        else:
+            self.add_virtual_pixel(rgb) #rgb is [r,g,b]
+        #print(rgb)
+    def add_virtual_pixel(self,rgb):
+        self.current_virtual_pixel+=1
+        if self.current_virtual_pixel==self.virtual_pixels_per_frame:
+            self.current_virtual_pixel=0
+            self.craft_frame(self.frames)
+            self.frames+=1
+            
+    def add_parity_virtual_pixel(self,frame_number):
+        pass
+    def craft_frame(self,frame_number):
+        pass
+
         
 
 
@@ -54,10 +70,11 @@ initial_size=len(data) #data is a list of 8 bit integers (0 to 255)
 data.extend([0]*(len(data)%3)) #if not a multiple of 3 pad with zeros and include the information on the Result class
 #print(data)
 
-data_combined_to_24_bits=list(zip(*[iter(data)]*3)) #group bytes in groups of 3 (24 bits, so they can be splitted to packets of 6 bits)
+data_combined_to_24_bits=list(zip(*[iter(data)]*3)) #group bytes in groups of 3 (24 bits, so they can be splitted to packets of 6 bits) in a tuple
 #print(min(data),max(data))
 
 final_video=Result(1920,1080,60,500000,(len(data)%3),initial_size)
-for packet_of_24_bits in data_combined_to_24_bits:
+for packet_of_24_bits in data_combined_to_24_bits[0:]:
+    #print("packet is: ",packet_of_24_bits)
     final_video.put_packet_in(packet_of_24_bits)
-    print(packet_of_24_bits)
+    
